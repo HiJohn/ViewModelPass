f77acae4ced4d504bb70f303d0d0d302771d72d3
gaozhipeng <gaozhipeng@meetstarinc.com>
2020-03-13 14:41:01 +0800

    test

---
+++ app/src/main/java/me/viewmodelpass/HoActivity.java
@@ -0,0 +1,14 @@
+package me.viewmodelpass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ho);
+    }
+}
